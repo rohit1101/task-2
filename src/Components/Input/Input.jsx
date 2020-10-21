254d4c3803ad944895fae974fa32e6eaf457c1a0
rohit1101 <srohit1101@gmail.com>
2020-10-21 17:33:21 +0530

    Style Input.jsx

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,15 +1,16 @@
 import React from "react"
-
+import styles from "./Input.module.scss"
 function Input({ search, searchHandler }) {
   return (
-    <div>
-      <input
-        type="text"
-        value={search}
-        onChange={searchHandler}
-        placeholder="&#x1F50D; Search"
-      />
-      <button>Sign In</button>
+    <div className={styles.wrap}>
+      <div className={styles.container}>
+        <input
+          type="text"
+          value={search}
+          onChange={searchHandler}
+          placeholder="&#x1F50D; Search"
+        />
+      </div>
       <hr />
     </div>
   )
